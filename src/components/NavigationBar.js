7e403356b79e7940c39a37b7d4164d58625d67f4
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-16 11:17:43 -0700

    NavigationBar tweeks

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,31 +7,31 @@ const NavigationBar = () => {
         <header class="navbar navbar-expand navbar-dark bg-primary shadow flex-column flex-md-row bd-navbar">
 
             <nav class="collapse navbar-collapse" >
-                <a class="navbar-brand" title="Beer Run" href="#">
+                <NavLink exact to="/" class="navbar-brand" title="Beer Run">
                     <i class="fas fa-beer"/>
-                    <span class="d-none d-sm-none d-md-inline">Beer Run</span>
-                </a>
+                    <span class="d-none d-sm-none d-md-inline"> Beer Run</span>
+                </NavLink>
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" title="Breweries" href="#">
+                        <NavLink exact to="/" class="nav-link" title="Breweries">
                             <i class="fas fa-industry"/>
-                            <span class="d-none d-sm-none d-md-inline">Breweries</span>
-                        </a>
+                            <span class="d-none d-sm-none d-md-inline"> Breweries</span>
+                        </NavLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" title="Circuits" href="#">
+                        <NavLink exact to="/" class="nav-link" title="Circuits">
                             <i class="fas fa-route"/>
-                            <span class="d-none d-sm-none d-md-inline">Circuits</span>
-                        </a>
+                            <span class="d-none d-sm-none d-md-inline"> Circuits</span>
+                        </NavLink>
                     </li>
                 </ul>
 
                 <ul class="navbar-nav ml-md-auto">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" title="Create Account" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <NavLink exact to="/" class="nav-link dropdown-toggle" title="Create Account" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-user-plus"/>
-                            <span class="d-none d-sm-none d-md-inline">Create Account</span>
-                        </a>
+                            <span class="d-none d-sm-none d-md-inline"> Create Account</span>
+                        </NavLink>
                         <form class="dropdown-menu dropdown-menu-right p-4 signup-login-form">
                             <div class="form-row">
                                 <div class="form-group col-sm-6">
@@ -52,15 +52,15 @@ const NavigationBar = () => {
                             </div>
                             <div class="form-row">
                                 <div class="col-6">
-                                    <button class="btn btn-block btn-primary" href="#">
+                                    <button class="btn btn-block btn-primary">
                                         <i class="fab fa-google"/>
-                                        <span class="d-none d-sm-none d-md-inline">Sign up with Google</span>
+                                        <span class="d-none d-sm-none d-md-inline"> Sign up with Google</span>
                                     </button>
                                 </div>
                                 <div class="col-6">
                                     <button type="submit" class="btn btn-block btn-success">
                                         <i class="fas fa-user-plus"/>
-                                        <span class="d-none d-sm-none d-md-inline">Create Account</span>
+                                        <span class="d-none d-sm-none d-md-inline"> Create Account</span>
                                     </button>
                                 </div>
                             </div>
@@ -68,10 +68,10 @@ const NavigationBar = () => {
                     </li>
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" title="Login" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <NavLink exact to="/" class="nav-link dropdown-toggle" title="Login" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-sign-in-alt"/>
-                            <span class="d-none d-sm-none d-md-inline">Login</span>
-                        </a>
+                            <span class="d-none d-sm-none d-md-inline"> Login</span>
+                        </NavLink>
                         <form class="dropdown-menu dropdown-menu-right p-4 signup-login-form">
                             <div class="form-group">
                                 <input type="email" id="email" class="form-control" placeholder="Email Address" />
@@ -82,22 +82,22 @@ const NavigationBar = () => {
                                 </div>
                                 <div class="form-group col-4">
                                     <button type="button" class="btn btn-block btn-primary">
-                                        <span class="d-none d-sm-none d-md-inline">Forgot</span>
+                                        <span class="d-none d-sm-none d-md-inline"> Forgot</span>
                                         <i class="far fa-question-circle"/>
                                     </button>
                                 </div>
                             </div>
                             <div class="form-row justify-content-center">
                                 <div class="col-6">
-                                    <button class="btn btn-block btn-primary" href="#">
+                                    <button class="btn btn-block btn-primary">
                                         <i class="fab fa-google"/>
-                                        <span class="d-none d-sm-none d-md-inline">Login with Google</span>
+                                        <span class="d-none d-sm-none d-md-inline"> Login with Google</span>
                                         </button>
                                 </div>
                                 <div class="col-6">
                                     <button type="submit" class="btn btn-block btn-success">
                                         <i class="fas fa-sign-in-alt"/>
-                                        <span class="d-none d-sm-none d-md-inline">Login to Beer Run</span>
+                                        <span class="d-none d-sm-none d-md-inline"> Login to Beer Run</span>
                                     </button>
                                 </div>
                             </div>
@@ -105,18 +105,18 @@ const NavigationBar = () => {
                     </li>
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" title="User Name" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <NavLink exact to="/" class="nav-link dropdown-toggle" title="User Name" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-running"/>
-                            <span class="d-none d-sm-none d-md-inline">User Name</span>
-                        </a>
+                            <span class="d-none d-sm-none d-md-inline"> User Name</span>
+                        </NavLink>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <h6 class="dropdown-header"><i class="fas fa-bookmark"/> Bookmarked</h6>
-                            <a class="dropdown-item" href="#"><i class="fas fa-industry"/> Breweries</a>
-                            <a class="dropdown-item" href="#"><i class="fas fa-route"/> Circuits</a>
+                            <NavLink exact to="/" class="dropdown-item"><i class="fas fa-industry"/> Breweries</NavLink>
+                            <NavLink exact to="/" class="dropdown-item"><i class="fas fa-route"/> Circuits</NavLink>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#"><i class="fas fa-address-card"/> Account</a>
+                            <NavLink exact to="/" class="dropdown-item"><i class="fas fa-address-card"/> Account</NavLink>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"/> Logout</a>
+                            <NavLink exact to="/" class="dropdown-item"><i class="fas fa-sign-out-alt"/> Logout</NavLink>
                         </div>
                     </li>
                 </ul>
