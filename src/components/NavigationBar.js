ec0eaa70e5c687899259ada70dd55aa5c7213d4c
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-16 10:56:35 -0700

    Remove reactstrap, doesnt fully support bootstrap

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,54 +1,11 @@
 import React from 'react'
-// import {NavLink} from 'react-router-dom';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText
-  } from 'reactstrap';
+import {NavLink} from 'react-router-dom';
+
 
 const NavigationBar = () => {
     return (
         <div>
-        <Navbar color="primary" light expand="md">
-          <NavbarBrand href="/">Beer Run</NavbarBrand>
-          <Collapse isOpen={true} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-            <NavbarText>Simple Text</NavbarText>
-          </Collapse>
-        </Navbar>
+       
       </div>
     )
 }
