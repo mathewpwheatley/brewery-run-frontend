ec277622790a9bed2e9b8dcd87c66c85a67779c3
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-17 15:37:06 -0700

    npm install redux-devtools, connected thunk

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import App from './App.js'
 import rootReducer from './reducers/index.js'
 import * as serviceWorker from './serviceWorker'
 
 // Create redux store, the two window options connect to the redux chrome extension and the last is for asynchronous dispatch
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() && applyMiddleware(thunk))
+const composeEnhancers = composeWithDevTools({})
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
+// Render App, Provider connects the store to the App
 ReactDOM.render(
   <Provider store={store}>
     <App />
