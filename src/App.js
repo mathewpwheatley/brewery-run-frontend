d87dac7fce693056aae9033f5864103144d86240
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-17 17:54:06 -0700

    Added create user and login routes to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route} from 'react-router-dom'
 import NavigationBar from './components/NavigationBar.js'
 import Breweries from './containers/Breweries.js'
 import Circuits from './containers/Circuits.js'
+import CreateAccountForm from './components/CreateAccountForm.js'
+import LogInForm from './components/LogInForm'
 import TestFetch from './containers/TestFetch.js'
 import LogOutForm from './components/LogOutForm.js'
 
@@ -14,6 +16,8 @@ function App() {
       <Route path="/breweries" component={Breweries} />
       <Route path="/circuits" component={Circuits} />
       <Route exact path="/test-fetch" component={TestFetch} />
+      <Route path="/create-account" component={CreateAccountForm} />
+      <Route path="/log-in" component={LogInForm} />
       {/* <Route exact path="/log-in" component={Login} /> */}
       {/* <Route exact path="/create-account" component={CreateAccount} /> */}
       <Route exact path="/log-out" component={LogOutForm} />
