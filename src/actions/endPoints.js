a1a92aca5a557b15bfea3553880ea03f3c30e4fc
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-08-19 21:43:59 -0700

    Updates endpoints forward slash positions

--- src/actions/endPoints.js
+++ src/actions/endPoints.js
@@ -1,22 +1,22 @@
-const baseURL = "https://brewery-run-backend.herokuapp.com"
-// const baseURL = "http://localhost:3001"
+const baseURL = "https://brewery-run-backend.herokuapp.com/"
+// const baseURL = "http://localhost:3001/"
 
 const endPoints = {
-    logInURL: baseURL + "/log_in",
-    autoLogInURL: baseURL + "/auto_log_in",
-    logOutURL: baseURL + "/log_out",
-    usersURL: baseURL + "/users",
-    breweriesURL: baseURL + "/breweries",
-    breweriesIndexFormURL: baseURL + "/breweries_form_index",
-    breweryReviewsURL: baseURL + "/brewery_reviews",
-    circuitsURL: baseURL + "/circuits",
-    circuitReviewsURL: baseURL + "/circuit_reviews",
-    notificationsURL: baseURL + "/notifications",
-    followsURL: baseURL + "/follows",
-    breweryLikesURL: baseURL + "/brewery_likes",
-    circuitLikesURL: baseURL + "/circuit_likes",
-    breweryFavoritesURL: baseURL + "/brewery_favorites",
-    circuitFavoritesURL: baseURL + "/circuit_favorites"
+    logInURL: baseURL + "log_in",
+    autoLogInURL: baseURL + "auto_log_in",
+    logOutURL: baseURL + "log_out",
+    usersURL: baseURL + "users",
+    breweriesURL: baseURL + "breweries",
+    breweriesIndexFormURL: baseURL + "breweries_form_index",
+    breweryReviewsURL: baseURL + "brewery_reviews",
+    circuitsURL: baseURL + "circuits",
+    circuitReviewsURL: baseURL + "circuit_reviews",
+    notificationsURL: baseURL + "notifications",
+    followsURL: baseURL + "follows",
+    breweryLikesURL: baseURL + "brewery_likes",
+    circuitLikesURL: baseURL + "circuit_likes",
+    breweryFavoritesURL: baseURL + "brewery_favorites",
+    circuitFavoritesURL: baseURL + "circuit_favorites"
 }
 
 export default endPoints
