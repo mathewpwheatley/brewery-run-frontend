9e83a05fe7fe03cd6352a8ea84dfc90e06507e76
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-09-16 11:35:42 -0700

    Add circuit leg label to circuit table

--- src/components/CircuitLegsTable.js
+++ src/components/CircuitLegsTable.js
@@ -1,36 +1,40 @@
 import React from 'react'
-import {Table} from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 
-const CircuitLegsTable = ({legs}) => {
+const CircuitLegsTable = ({ legs }) => {
+  const nextChar = (c) => {
+    return String.fromCharCode(c.charCodeAt(0) + 1)
+  }
 
-    const mapTableRows = () => {
-        return (
-            legs.map(leg => {
-                return (
-                    <tr>
-                        <td>{leg.start_address}</td>
-                        <td>{leg.end_address}</td>
-                        <td>{leg.distance}</td>
-                    </tr>
-                )
-            })
-        )
-    }
+  const mapTableRows = () => {
+    let currWaypoint
+    return legs.map((leg) => {
+      currWaypoint = !!currWaypoint ? nextChar(currWaypoint) : 'A'
+      let nextWaypoint = nextChar(currWaypoint)
+      return (
+        <tr>
+          <td>{currWaypoint + ' - ' + nextWaypoint}</td>
+          <td>{leg.start_address}</td>
+          <td>{leg.end_address}</td>
+          <td>{leg.distance}</td>
+        </tr>
+      )
+    })
+  }
 
-    return (
-        <Table className="text-center" size="sm" hover bordered>
-            <thead className="thead-light">
-                <tr>
-                    <th>Start Address</th>
-                    <th>End Address</th>
-                    <th>Distance</th>
-                </tr>
-            </thead>
-            <tbody>
-                {mapTableRows()}
-            </tbody>
-        </Table>
-    )
+  return (
+    <Table className='text-center' size='sm' hover bordered>
+      <thead className='thead-light'>
+        <tr>
+          <th>Circuit Leg</th>
+          <th>Start Address</th>
+          <th>End Address</th>
+          <th>Distance</th>
+        </tr>
+      </thead>
+      <tbody>{mapTableRows()}</tbody>
+    </Table>
+  )
 }
 
 export default CircuitLegsTable
